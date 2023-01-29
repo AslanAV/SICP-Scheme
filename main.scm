29eb36bc58899770d0d6ea341f3ee88eb51193dc
Aslan Autlev <autlevaslan@gmail.com>
2023-01-29 23:03:56 +0000

    lesson 1

--- main.scm
+++ main.scm
@@ -1,4 +1,33 @@
 (display "hello world")
 (newline)
-(sum-of-squares (+ 5 1) (+ 5 2))
-(+ (square 6) (square 7))
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+(define (abs x)
+  (if (< x 0) (- x) x))
+
+(define (>= x y)
+  (or (> x y) (= x y)))
+
+(define (>= x y)
+  (not (< x y)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x) guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.01))
+
+(define (mysqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (square guess)
+  (* guess guess))
